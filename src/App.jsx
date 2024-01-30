8e6163228b0d295544a869c240fd477d3ddb4cb2
saminyasar004 <yasarsamin57@gmail.com>
2024-01-30 12:15:06 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import "./styles/ionic_variables.css";
 
 import Welcome from "./pages/Welcome";
 import ChatList from "./pages/ChatList";
+import Inbox from "./pages/Inbox";
 
 setupIonicReact();
 
@@ -36,6 +37,9 @@ const App = () => (
                 </Route>
                 <Route exact path="/chat">
                     <ChatList />
+                </Route>
+                <Route exact path="/inbox">
+                    <Inbox />
                 </Route>
             </IonRouterOutlet>
         </IonReactRouter>
